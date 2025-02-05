730c074023e6866042d13039996993bfb4d0d8f2
AlexDerAndros <ntrikos@gmx.de>
2025-02-05 23:39:13 +0100

    Header + React Router

--- ntr/src/App.js
+++ ntr/src/App.js
@@ -1,9 +1,44 @@
 import './App.css';
+import {Link,Routes, BrowserRouter, Route} from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
 
 export default function App() {
   return (
-    <div className='main'>
-      Ntrikos
+   <BrowserRouter>
+     <main>
+      <div className='header'>
+       Familie Ntrikos
+      </div>
+      <Link to='/ToDoListe'>
+       <button>
+         To Do Liste
+       </button>
+      </Link>
+      <Link to='/'>
+       <button>
+         Startseite
+       </button>
+      </Link>
+     </main>
+     <Routes>
+      <Route path='/' element={<Startseite/>}/>
+      <Route path='/ToDoListe' element={<ToDoListe/>}/>
+     </Routes>
+    </BrowserRouter> 
+  );
+}
+function Startseite() {
+  return (
+    <div>
+      Startseite
     </div>
+  );
+}
+function ToDoListe() {
+  return (
+   <div>
+     To Do Liste
+   </div>
   );
 }
